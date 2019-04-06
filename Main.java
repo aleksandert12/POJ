facfe4adccbbfd2c5ff1fcbdf153b338bb52411a
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-04-06 15:56:16 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
   Employee Kacper = new Employee(1,"Kacper","Wieczorek",5000);
-        System.out.println("Kacper "+Kacper.toString());
+        InvoiceItem Aleksander = new InvoiceItem(1,"opis",5000,2);
+        System.out.println(Kacper);
+        System.out.println(Aleksander);
           }
 }
