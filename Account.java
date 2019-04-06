3430b74a7d434a7678ee86394b1e158adeab5bbd
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-04-06 22:13:10 +0200

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,50 @@
+public class Account {
+    private int id;
+    private String name;
+    private int balance;
+
+
+    public Account(int id, String name){
+        this.id=id;
+        this.name=name;
+        this.balance=0;
+
+    }
+    public Account (int id, String name, int balance){
+        this.id=id;
+        this.name=name;
+        this.balance=balance;
+    }
+    public int getid(){
+        return id;
+    }
+    public String getname(){
+        return name;
+    }
+    public int getbalance(){
+        return balance;
+    }
+    public int credit(int amount){
+        return balance+amount;
+    }
+    public int debit(int amount){
+        if(amount <= balance)
+            balance=amount-balance;
+        else
+            System.out.println("Przekroczona kwota");
+        return balance;
+    }
+    public int transferTo (int account  , int amount)
+    {
+        if (amount <= balance)
+            balance=account;
+        else
+            System.out.println("Przekoroczona kwota");
+        return balance;
+
+    }
+    public String toString()
+    {
+        return "Account[id= " + id + "  name  "+ name + " balance= " + balance + "]";
+    }
+}
