1a016c1b820345be1ccabb08af8bc2e430a6092a
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-04-27 20:55:53 +0200

    Update Main.java

--- LAB3/Main.java
+++ LAB3/Main.java
@@ -1,9 +1,8 @@
 /*
 Aleksander Tyński
 Kacper Wieczorek
-Lab3
-Klasa Main
- */
+Lab3 Klasa Main
+*/
 public class Main {
     public static void main(String[] args) {
         Employee Kacper = new Employee(1, "Kacper", "Wieczorek", 5000);
@@ -11,10 +10,17 @@ public class Main {
         Date Kiedys = new Date(28, 8, 1997);
         Account My = new Account(1,"Aleksander",1);
         Time Zegarek = new Time(5, 23, 45);
+        Palindrom n1= new Palindrom("Atak kata");
+        Palindrom n2= new Palindrom("Muzo, raz daj jad za rozum");
+        Palindrom n3= new Palindrom("Wódy żal dla żydów");
+        
         System.out.println(Kacper);
         System.out.println(Aleksander);
         System.out.println(Kiedys);
         System.out.println(Konto);
         System.out.println(Zegarek);
+        System.out.println(n1.getPalindrom()+" "+n1.sprawdz());
+        System.out.println(n2.getPalindrom()+" "+n2.sprawdz());
+        System.out.println(n3.getPalindrom()+" "+n3.sprawdz());
     }
 }
