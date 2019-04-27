127b5e75de4ea79832daa4d6b1dfd0e00e0e071c
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-04-27 21:14:15 +0200

    Create main.java

---
+++ Lab4/zadanie 2/main.java
@@ -0,0 +1,14 @@
+/*
+* Autorzy: Martyna Klebba
+*           Aleksander Tyński
+* Zadnie 2 POINT2D
+* */
+public class Main {
+
+    public static void main(String[] args) {
+        Point2D Olek = new Point2D(5.6f,7.8f);
+        Point3D Marta = new Point3D(7.6f,5.4f,4.9f);
+        System.out.println(Olek);
+        System.out.println(Marta);
+    }
+}
