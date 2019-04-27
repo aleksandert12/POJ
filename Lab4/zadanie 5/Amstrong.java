612938a896d59ebc67b9d9b67b15af20ab01e3e6
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-04-27 21:29:45 +0200

    Create  Amstrong.java

---
+++ Lab4/zadanie 5/Amstrong.java
@@ -0,0 +1,31 @@
+/*
+Autorzy: Aleksander Tyński
+         Martyna Klebba
+         Zadanie 5 liczby Amstronga
+*/
+public class Armstrong {
+
+    public static void main(String[] args) {
+
+        int liczba = 153;
+
+        int pocz_liczba, reszta, wynik = 0, n = 0;
+
+        pocz_liczba = liczba;
+
+        for (;pocz_liczba != 0; pocz_liczba /= 10, ++n);
+
+        pocz_liczba = liczba;
+
+        for (;pocz_liczba != 0; pocz_liczba /= 10)
+        {
+            reszta = pocz_liczba % 10;
+            wynik += Math.pow(reszta, n);
+        }
+
+        if(wynik == liczba)
+            System.out.println(liczba + " is an Armstrong number.");
+        else
+            System.out.println(liczba + " is not an Armstrong number.");
+    }
+}
