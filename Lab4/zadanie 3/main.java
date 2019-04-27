d9ef6038573c8aca664221375134f1a23ed1cae8
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-04-27 21:19:13 +0200

    Create  main.java

---
+++ Lab4/zadanie 3/main.java
@@ -0,0 +1,16 @@
+/*
+    Martyna Klebba
+    Aleksander Tyński
+    Zadanie 3 lab4
+    Point/MovablePoint
+     */
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Point Marta=new Point (2f,5f);
+        MovablePoint Olek = new MovablePoint(1.5f,1.8f);
+        System.out.println(Marta);
+        System.out.println(Olek);
+    }
+}
