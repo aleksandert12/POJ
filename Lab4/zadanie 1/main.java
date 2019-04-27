2ce19bae733b9e0f2cfd0983a9ae0a40d202b607
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-04-27 21:06:25 +0200

    Create  main.java

---
+++ Lab4/zadanie 1/main.java
@@ -0,0 +1,19 @@
+/ *
+Autorzy: Aleksander Tyński
+        Martyna Klebba
+   Zadnie nr 1 - osoba
+* /
+
+ klasa  publiczna Main {
+
+
+    public  static  void  main ( String [] args )
+    {
+        osoba Olek  =  nowa osoba ( „ Aleksandra ” , „ Gdynia ” );
+        student Marta  =  nowa studentka ( „ Martyna ” , „ Reda ” , „ informatyka ” , 1 , 650 );
+        personel OlMart  =  nowy personel ( „ Aleksandra ” , „ Reda ” , „ PJATK ” , 6000 );
+        System . na zewnątrz . println ( Olek );
+        System . na zewnątrz . println ( Marta );
+        System . na zewnątrz . println ( OlMart );
+    }
+}
