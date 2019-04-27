53df2669ecc6cec6921764b2c03adb16eef6aa79
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-04-27 21:07:34 +0200

    Create  person.java

---
+++ Lab4/zadanie 1/person.java
@@ -0,0 +1,33 @@
+/ *
+Autorzy: Aleksander Tyński
+        Martyna Klebba
+   Zadnie nr 1 - osoba
+* /
+
+ osoba z klasy  publicznej {
+
+    private  String nazwa;
+    prywatny  adres String ;
+
+    publiczna  osoba ( String  name , String  adres )
+    {
+        to . nazwa = nazwa;
+        to . adres = adres;
+    }
+    public  String  getName ()
+    {
+        zwróć nazwę;
+    }
+    public  String  getAddress ()
+    {
+        adres zwrotny ;
+    }
+    public  void  setAddress ( String  address )
+    {
+        to . adres = adres;
+    }
+    public  String  toString ()
+    {
+        return  „ Osoba [imie: „  + nazwa +  ” , Adres: „  + adres +  ” ] ” ;
+    }
+}
