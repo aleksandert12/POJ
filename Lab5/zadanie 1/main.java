cf844eec2b3af2beac4068d55d0cefb61d427dd7
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-05-11 15:33:58 +0200

    Create main.java

---
+++ Lab5/zadanie 1/main.java
@@ -0,0 +1,17 @@
+/*
+Autorzy: Aleksander Tyński
+         Maciej Falkiewicz
+   Zadnie nr 1 - MyPoint
+*/
+
+public class Main {
+
+
+    public static void main (String [] args)
+    {
+        MyPoint Olek = new MyPoint (12 ,15);
+
+        System.out.println(Olek);
+
+    }
+}
