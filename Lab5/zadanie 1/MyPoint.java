6bb4cf3ec1462dd2c5afe8321b1e1ebc1b1e829e
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-05-11 19:14:49 +0200

    Update MyPoint.java

--- Lab5/zadanie 1/MyPoint.java
+++ Lab5/zadanie 1/MyPoint.java
@@ -5,40 +5,68 @@ Autorzy: Aleksander Tyński
 */
 
 public class MyPoint {
-    private int x;
-    private int y;
+    private int x = 0;
+    private int y = 0;
 
-    public MyPoint (int x, int y)
-    {
-        this.x=x;
-        this.y=y;
+    public MyPoint() {
     }
+
+    public MyPoint(int x, int y) 
+    {
+        this.x = x;
+        this.y = y;
+    }
+
     public int getX()
     {
         return x;
     }
-    public void setX (int x)
+
+    public void setX(int x) 
     {
-        this.x=x;
+        this.x = x;
     }
+
     public int getY()
     {
         return y;
     }
-    public void setY (int y)
-    {
-        this.y=y;
-    }
-    public int [] getxy={x,y};
 
-    public void setXY(int x, int y)
+    public void setY(int y)
     {
-        this.x=x;
-        this.y=y;
+        this.y = y;
     }
+
+    public int[] getXY = {x, y};
+
+    public void setXY(int x, int y) 
+    {
+        this.x = x;
+        this.y = y;
+    }
+
     public String toString()
     {
-        return "x: " + x + "y: " + y;
+        return "(" + x + "," + y + ")";
     }
 
-}
+    public double distance(int x, int y) 
+    {
+        int xdis = this.x - x;
+        int ydis = this.y - y;
+        return Math.sqrt(xdis * xdis + ydis * ydis);
+    }
+
+    public double distance(MyPoint another)
+    {
+        int xdis = this.x - another.x;
+        int ydis = this.y - another.y;
+        return Math.sqrt(xdis * xdis + ydis * ydis);
+    }
+
+    public double distance() 
+    {
+        int xdis = this.x - 0;
+        int ydis = this.y - 0;
+        return Math.sqrt(xdis * xdis + ydis * ydis);
+    }
