286faff047f27fe081f25a605858c135379d416a
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-05-11 21:24:41 +0200

    Create Invoice.java

---
+++ Lab5/zadanie 5/Invoice.java
@@ -0,0 +1,42 @@
+/*
+    Aleksander Tyński
+     Maciej Falkiewicz
+    Zadanie 5 lab5
+    Klasa  Invoice
+     */
+public class Invoice {
+    private int id;
+    private Customer customer;
+    private double amount;
+
+    public Invoice(int id,Customer customer,double amount)
+    {
+        this.id=id;
+        this.customer=customer;
+        this.amount=amount;
+    }
+    public int getID()
+    {
+        return id;
+    }
+    public Customer getCustomer()
+    {
+        return customer;
+    }
+    public void setCustomer(Customer customer) 
+    {
+        this.customer = customer;
+    }
+    public String getAmount()
+    {
+        return Double.toString(amount);
+    }
+    public String getCustomerName()
+    {
+        return customer.getName();
+    }
+    public double getAmountAfterDiscount()
+    {
+        return amount*(1.0d-(customer.getDiscount()/100.0d));
+    }
+}
