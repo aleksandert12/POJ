b965db62d79d8c35692ede72c00f4da9ab0aed1c
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-05-25 15:30:19 +0200

    Create Main.java

---
+++ Lab6/Zadanie1/Main.java
@@ -0,0 +1,19 @@
+/*
+Autorzy: Aleksander Tyński
+        Dorota Matkowska
+   Zadnie nr 1 - Movable
+   Lab 6
+*/
+public class Main {
+
+
+    public static void main (String [] args)
+    {
+        MovablePoint Olek = new MovablePoint(12, 23, 53, 34);
+        MovableRectangle Dorota = new MovableRectangle(23,24, 23, 23, 23, 45) ;
+
+        System.out.println(Olek);
+        System.out.println(Dorota);
+
+    }
+}
