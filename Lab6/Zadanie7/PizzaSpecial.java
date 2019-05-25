b398e023fefa8a923ad3c0d36cd54811ef705573
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-05-25 21:48:13 +0200

    Create PizzaSpecial.java

---
+++ Lab6/Zadanie7/PizzaSpecial.java
@@ -0,0 +1,36 @@
+/*
+    Tyński Aleksander
+    Dorota Matkowska
+    Zadanie 7 lab6
+     */
+import java.util.Random;
+
+public class PizzaSpecial extends Pizza {
+    Random rand = new Random();
+    protected double calories;
+    protected String name;
+    protected double price;
+    public PizzaSpecial(double calories, String name, double price)
+    {
+        super(calories,name,price);
+        super.calories=calories;
+        super.name=name;
+        super.price=price;
+    }
+    public PizzaSpecial()
+    {
+        super();
+    }
+
+    public void setCalories(double calories) {
+        this.calories = 300+rand.nextInt(1000);
+    }
+    public void setName(String name)
+    {
+        this.name="Pizza Special";
+    }
+    public void setPrice(double price)
+    {
+        this.price=rand.nextInt(25);
+    }
+}
