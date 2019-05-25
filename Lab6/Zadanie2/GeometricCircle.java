50bfb4e71c2d28263e8e100a44e24e0525b40d9b
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-05-25 15:43:18 +0200

    Create GeometricCircle.java

---
+++ Lab6/Zadanie2/GeometricCircle.java
@@ -0,0 +1,12 @@
+/*
+Autorzy: Aleksander Tyński
+        Dorota Matkowska
+   Zadanie 2 - Circle
+   Lab 6
+*/
+public interface GeometricObject {
+
+    public double getPerimeter();
+    public double getArea();
+
+}
