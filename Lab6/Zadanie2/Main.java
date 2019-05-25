b4f57c80ff2a7a6d1c5490c990856e568e89e714
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-05-25 15:42:10 +0200

    Create Main.java

---
+++ Lab6/Zadanie2/Main.java
@@ -0,0 +1,15 @@
+/*
+Autorzy: Aleksander Tyński
+        Dorota Matkowska
+   Zadanie 2 - Circle
+   Lab 6
+*/
+public class Main {
+
+
+    public static void main (String [] args)
+    {
+        Circle Olek = new Circle(13);
+        System.out.println(Olek);
+    }
+}
