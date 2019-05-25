a1c31c4d8ecdd81a3b8ab987508a91a63e28bd1c
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-05-25 16:23:53 +0200

    Create UsedCarException.java

---
+++ Lab6/Zadanie8/UsedCarException.java
@@ -0,0 +1,14 @@
+/*
+    Dorota Matkowska
+    Aleksander Tyński
+    Zadanie 3 lab6
+    Klasa Health
+     */
+     public class UsedCarException extends Exception {
+
+
+    public UsedCarException( String vinNum)
+    {
+        super("Wrong info entered");
+    }
+}
