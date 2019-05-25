98e4e1f1275976fd2bd80b074e64546e9f2bb9b5
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-05-25 20:20:12 +0200

    Create Machine.java

---
+++ Lab6/Zadanie4/Machine.java
@@ -0,0 +1,14 @@
+/*
+Autorzy: Aleksander Tyński
+        Dorota Matkowska
+   Zadanie 4 - Runner
+   Lab 6
+*/
+
+
+public class Machine implements Runner {
+    @Override
+    public void run() {
+        System.out.println("Machine operates using electricity");
+    }
+}
