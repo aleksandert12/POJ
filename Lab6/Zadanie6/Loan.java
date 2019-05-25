2d5bb09f74b870ba3b6f00d8b729d14b27a1a6a2
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-05-25 20:50:34 +0200

    Update Loan.java

--- Lab6/Zadanie6/Loan.java
+++ Lab6/Zadanie6/Loan.java
@@ -16,18 +16,20 @@ public abstract class Loan implements LoanConstants {
         this.loanNum = loanNum;
         this.lastName = lastName.toUpperCase();
 
-        // validates value of loan amount
-        try {
-            if (loanAmt > MAX_LOAN_AMOUNT) {
+        try 
+        {
+            if (loanAmt > MAX_LOAN_AMOUNT)
+            {
                 throw (new InputMismatchException("Loan amount value is over $100,000.00"));
             } else {
                 this.loanAmt = loanAmt;
             }
-        } catch (InputMismatchException e) {
+        }
+        catch (InputMismatchException e) 
+        {
             System.out.println(e.getMessage());
         }
 
-        // sets term default to 1-year loan if loan term is not 
         switch (term) {
             case MEDIUM_TERM:
             case LONG_TERM:
@@ -42,7 +44,8 @@ public abstract class Loan implements LoanConstants {
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return String.format("%s\t\t%s\t\t$%,.2f\t%.2f%%\t\t%d\t\t$%,.2f",
                 this.loanNum, this.lastName, this.loanAmt, this.interestRate * 100,
                 this.term, this.loanAmt + (this.loanAmt * this.interestRate));
