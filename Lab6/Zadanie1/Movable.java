a5e39af9815f7f86e0f654f5b391e5783f646909
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-05-25 15:31:05 +0200

    Create Movable.java

---
+++ Lab6/Zadanie1/Movable.java
@@ -0,0 +1,14 @@
+/*
+Autorzy: Aleksander Tyński
+        Dorota Matkowska
+   Zadnie nr 1 - Movable
+   Lab 6
+*/
+public interface Movable {
+
+    public void moveUp();
+    public void moveDown();
+    public void moveLeft();
+    public void moveRight();
+
+}
