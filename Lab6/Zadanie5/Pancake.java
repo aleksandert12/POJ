04f07b1dd175d5119f84e1bbad6cc86147b0cc2c
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-05-25 20:29:28 +0200

    Create Pancake.java

---
+++ Lab6/Zadanie5/Pancake.java
@@ -0,0 +1,12 @@
+/*
+    Tyński Aleksander
+    Dorota Matkowska
+    Zadanie 5 lab6
+    Interface Turner
+     */
+public class Pancake implements Turner {
+    @Override
+    public void turn() {
+        System.out.println("Flipping");
+    }
+}
