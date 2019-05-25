1256383e85bb4d2bd93e1a2d1ace4f25b6af7736
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-05-25 20:20:47 +0200

    Create PoliticalCandidate.java

---
+++ Lab6/Zadanie4/PoliticalCandidate.java
@@ -0,0 +1,14 @@
+/*
+Autorzy: Aleksander Tyński
+        Dorota Matkowska
+   Zadanie 4 - Runner
+   Lab 6
+*/
+
+
+public class PoliticalCandidate implements Runner {
+    @Override
+    public void run() {
+        System.out.println("Political Candidate runs in elections to become president");
+    }
+}
