8054c7d77c961a872de95ddd6b80c2afbba7f7e4
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-05-25 20:19:19 +0200

    Create Athlete.java

---
+++ Lab6/Zadanie4/Athlete.java
@@ -0,0 +1,13 @@
+/*
+Autorzy: Aleksander Tyński
+        Dorota Matkowska
+   Zadanie 4 - Runner
+   Lab 6
+*/
+
+public class Athlete implements Runner {
+    @Override
+    public void run() {
+        System.out.println("Athlete literally runs on his feet to be the fastest");
+    }
+}
