6352db445dac4e84e1e5eb8676f695c31e5093bb
aleksandert12 <48859039+aleksandert12@users.noreply.github.com>
2019-06-08 22:30:08 +0200

    Create UATicketInfo.java

---
+++ Lab7/Zadanie1/UATicketInfo.java
@@ -0,0 +1,13 @@
+/*
+ Autorzy: Tyński Aleksander 
+             Michał Kaczmarek
+      Lab7  Zadanie 1
+     */
+import java.time.LocalDateTime;
+
+public class UATicketInfo {
+    String from;
+    String to;
+    LocalDateTime dateTime;
+    double price;
+}
